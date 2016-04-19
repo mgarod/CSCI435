a592984b1d338cd68814336805c1054fab60b6a5
Michael Garod <mgarod@gmail.com>
2016-04-19 19:10:10 -0400

    Initial Commit

---
+++ create_munitions.sql
@@ -0,0 +1,65 @@
+CREATE TABLE Munitions(
+	munition_id NUMBER PRIMARY KEY,
+	m_cost NUMBER,
+	m_name VARCHAR(20)
+);
+
+INSERT INTO Munitions VALUES(
+	1,
+	7000,
+	'AK-47'
+);
+
+INSERT INTO Munitions VALUES(
+	2,
+	3500,
+	'MP5'
+);
+
+INSERT INTO Munitions VALUES(
+	3,
+	2500,
+	'Mac-10'
+);
+
+INSERT INTO Munitions VALUES(
+	4,
+	6000,
+	'XM-1108'
+);
+
+INSERT INTO Munitions VALUES(
+	5,
+	5500,
+	'FAMAS'
+);
+
+INSERT INTO Munitions VALUES(
+	6,
+	200,
+	'Flashbang'
+);
+
+INSERT INTO Munitions VALUES(
+	7,
+	300,
+	'EMP Grenades'
+);
+
+INSERT INTO Munitions VALUES(
+	8,
+	325,
+	'Smoke Grenade'
+);
+
+INSERT INTO Munitions VALUES(
+	9,
+	800,
+	'Riot Shield'
+);
+
+INSERT INTO Munitions VALUES(
+	10,
+	750,
+	'Flak Jackets'
+);
---
+++ create_store.sql
@@ -0,0 +1,54 @@
+CREATE TABLE Store(
+	store_id NUMBER PRIMARY KEY,
+	store_name VARCHAR2(30)
+);
+
+INSERT INTO Store VALUES(
+	1,
+	'Walmart'
+);
+
+INSERT INTO Store VALUES(
+	2,
+	'K-Mart'
+);
+
+INSERT INTO Store VALUES(
+	3,
+	'BestBuy'
+);
+
+INSERT INTO Store VALUES(
+	4,
+	'Newegg'
+);
+
+INSERT INTO Store VALUES(
+	5,
+	'PC Richards'
+);
+
+INSERT INTO Store VALUES(
+	6,
+	'Target'
+);
+
+INSERT INTO Store VALUES(
+	7,
+	'Macy''s'
+);
+
+INSERT INTO Store VALUES(
+	8,
+	'Target'
+);
+
+INSERT INTO Store VALUES(
+	9,
+	'JCPenny'
+);
+
+INSERT INTO Store VALUES(
+	10,
+	'Amazon'
+);
