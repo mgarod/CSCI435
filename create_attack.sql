29312e0396079eb0e5bfc7f6b362b0ccf010f46b
Michael Garod <mgarod@gmail.com>
2016-05-05 17:45:45 -0400

    Add Attack Table with 1 example

---
+++ create_attack.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Attack(
+	attacker_id NUMBER NOT NULL,
+	defender_id NUMBER NOT NULL,
+	location VARCHAR2(30),
+	time timestamp NOT NULL,
+	victor_id NUMBER NOT NULL,
+	CONSTRAINT fk_attacker FOREIGN KEY (attacker_id)
+		REFERENCES Player (player_id),
+	CONSTRAINT fk_defender FOREIGN KEY (defender_id)
+		REFERENCES Player (player_id)
+);
+
+INSERT INTO ATTACK VALUES(1001, 1002, 'Croatia',
+	TO_TIMESTAMP('2016-01-01 18:10:15', 'YYYY-MM-DD HH24:MI:SS'), 1);
