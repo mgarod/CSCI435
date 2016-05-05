c66e2f31de5e9bec61629e38eba258aa5d4cf931
Michael Garod <mgarod@gmail.com>
2016-05-05 19:03:30 -0400

    Add make_attacks.py which generates toy attacks

---
+++ make_attacks.py
@@ -0,0 +1,38 @@
+import random as r
+
+Location = [
+	"Ukraine",
+	"Croatia",
+	"Nigeria",
+	"Laos",
+	"Nicaragua",
+	"Colombia",
+	"Turkey",
+	"Russia",
+	"North Korea"
+]
+
+Players = range(1001, 1010 + 1)
+
+def get_other_player(player):
+	while True:
+		other = r.choice(Players)
+		if other != player:
+			return other
+
+def get_loc():
+	return "\'"+r.choice(Location)+"\'"
+
+def make_date():
+	mon = str(r.choice(range(1,13)))
+	day = str(r.choice(range(1,28)))
+	hr = str(r.choice(range(0,25)))
+	m = str(r.choice(range(0,61)))
+	sec = str(r.choice(range(0,61)))
+	return "\'2015-"+mon+"-"+day+" "+hr+":"+m+":"+sec+"\'"
+
+
+for i in Players:
+	for j in range(r.randint(5,12)):
+		other = get_other_player(i)
+		print """INSERT INTO ATTACK VALUES({0}, {1}, {2},\n\t TO_TIMESTAMP({3}, \'YYYY-MM-DD HH24:MI:SS\'), {4});""".format(i, other, get_loc(), make_date(), r.choice([i, other]))
