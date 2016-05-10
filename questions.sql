c77d62384d4ab0c93105c8271583676c0af6476f
zouhairkhallaf <zouhairkhallaf@gmail.com>
2016-05-10 18:06:08 -0400

    Update questions.sql

--- questions.sql
+++ questions.sql
@@ -7,3 +7,8 @@ WHERE (defender_id=1001 OR attacker_id=1001) AND victor_id!=1001;
 -- Who is the player with the most wins? Least wins?
 -- What country has player X most often won?
 -- In what month did the most attacks take place?
+-- How many times Player X and Player Y have fought if so Where was the battle?
+-- What are the top 5 countries with the most number of battles?
+-- What is the most bloody battle and what is the most used munition used ?
+-- What's the name of the Player that orders the most grenades and when ?
+-- What is the player who ordered more munition than his opponent and yet lost the battle ? 
