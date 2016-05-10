06750e1f03c6474c972c057981bfa2e27b5c1aee
zouhairkhallaf <zouhairkhallaf@gmail.com>
2016-05-10 18:16:10 -0400

    Update questions.sql

--- questions.sql
+++ questions.sql
@@ -7,19 +7,11 @@ WHERE (defender_id=1001 OR attacker_id=1001) AND victor_id!=1001;
 -- Who is the player with the most wins? Least wins?
 -- What country has player X most often won?
 -- In what month did the most attacks take place?
--- How many times Player X and Player Y have fought?
-SELECT COUNT(*)
-FROM ATTACK
-WHERE (attacker_id=1004 AND defender_id=1006)
-	OR (attacker_id=1006 AND defender_id=1004)
-
--- Where have player X and player Y battle?
-SELECT attacker_id, defender_id, location
-FROM ATTACK
-WHERE (attacker_id=1004 AND defender_id=1006)
-	OR (attacker_id=1006 AND defender_id=1004)	
-
+-- How many times Player X and Player Y have fought if so Where was the battle?
 -- What are the top 5 countries with the most number of battles?
+SELECT location , COUNT(location) AS Frequency FROM attack GROUP BY location ORDER BY COUNT(location) DESC; 
+
 -- What is the most bloody battle and what is the most used munition used ?
 -- What's the name of the Player that orders the most grenades and when ?
 -- What is the player who ordered more munition than his opponent and yet lost the battle ? 
+-- 
